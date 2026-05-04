5a8014e6a40004c86ab2c85d303312f5efe586d7
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-05-04 02:19:26 -0500

    clases mejoradas

--- src/com/krakedev/artesanal/Negocio.java
+++ src/com/krakedev/artesanal/Negocio.java
@@ -1,5 +1,47 @@
 package com.krakedev.artesanal;
 
 public class Negocio {
+	private String nombre;
+	private Maquina maquinaA;
+	private int ultimoCodigo = 100;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Maquina getMaquinaA() {
+		return maquinaA;
+	}
+
+	public void setMaquinaA(Maquina maquinaA) {
+		this.maquinaA = maquinaA;
+	}
+
+	public Negocio() {
+	}
+
+	public Negocio(String nombre, Maquina maquinaA) {
+		this.nombre = nombre;
+		this.maquinaA = maquinaA;
+	}
+
+	public void asignarCodigoCliente(Cliente cliente) {
+		cliente.setCodigo(ultimoCodigo);
+		ultimoCodigo++;
+	}
+
+	public void cargarMaquinaA() {
+		maquinaA.llenarMaquina();
+	}
+
+	public void consumirCervezaMaquinaA(Cliente cliente, double ml) {
+		double valorCuenta = maquinaA.servirCerveza(ml);
+		cliente.setTotalConsumido(cliente.getTotalConsumido() + valorCuenta);
+		
+	}
 
 }
--- src/com/krakedev/artesanal/NegocioMejorado.java
+++ src/com/krakedev/artesanal/NegocioMejorado.java
@@ -1,5 +1,118 @@
 package com.krakedev.artesanal;
 
-public class NegocioMejorado {
+import java.util.ArrayList;
 
+public class NegocioMejorado {
+	private ArrayList<Maquina> maquinas;
+	private ArrayList<Cliente> clientes = new ArrayList<>();
+	private int ultimoCodigo = 100;
+
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+	public ArrayList<Maquina> getMaquinas() {
+		return maquinas;
+	}
+
+	public void setMaquinas(ArrayList<Maquina> maquinas) {
+		this.maquinas = maquinas;
+	}
+
+	public NegocioMejorado() {
+		maquinas = new ArrayList<Maquina>();
+	}
+
+	public String generarCodigo() {
+		int numerico = (int) (Math.random() * 100) + 1;
+
+		String codigo = "M-" + numerico;
+
+		return codigo;
+	}
+
+	public boolean agregarMaquina(String nombreCerveza, String descripcion, double precioPorMl) {
+		String codigo = generarCodigo();
+		Maquina m = recuperarMaquina(codigo);
+		if (m != null) {
+			return false;
+		} else {
+			Maquina taberna = new Maquina(codigo, nombreCerveza, descripcion, precioPorMl);
+			maquinas.add(taberna);
+			return true;
+		}
+	}
+
+	public void cargarMaquinas() {
+		for (int i = 0; i < maquinas.size(); i++) {
+			Maquina m = maquinas.get(i);
+			m.llenarMaquina();
+		}
+	}
+
+	public Maquina recuperarMaquina(String codigo) {
+		Maquina m;
+		for (int i = 0; i < maquinas.size(); i++) {
+			m = maquinas.get(i);
+			if (codigo.equals(m.getCodigo())) {
+				return m;
+			}
+		}
+		return null;
+	}
+
+	public void registrarCliente(String nombre, String cedula) {
+		Cliente cliente = new Cliente(nombre, cedula);
+		cliente.setCodigo(ultimoCodigo);
+		ultimoCodigo++;
+		clientes.add(cliente);
+	}
+
+	public Cliente buscarClientePorCedula(String cedula) {
+		Cliente c;
+		for (int i = 0; i < clientes.size(); i++) {
+			c = clientes.get(i);
+			if (cedula.equals(c.getCedula())) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public Cliente buscarClientePorCodigo(int codigo) {
+		Cliente c;
+		for (int i = 0; i < clientes.size(); i++) {
+			c = clientes.get(i);
+			if (codigo == c.getCodigo()) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public void consumirCerveza(int codigo, String codMaquina, double cantidad) {
+		Maquina m = recuperarMaquina(codMaquina);
+		Cliente c = buscarClientePorCodigo(codigo);
+		double valorConsumido = m.servirCerveza(cantidad);
+		registrarConsumo(c, valorConsumido);
+
+	}
+
+	public void registrarConsumo(Cliente cliente, double valorConsumido) {
+		double valorTotal = cliente.getTotalConsumido() + valorConsumido;
+		cliente.setTotalConsumido(valorTotal);
+	}
+
+	public double consultarValorVendido() {
+		double valorTotal = 0;
+		for (int i = 0; i < clientes.size(); i++) {
+			Cliente c = clientes.get(i);
+			valorTotal += c.getTotalConsumido();
+		}
+		return valorTotal;
+	}
 }
